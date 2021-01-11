706de8abe7c34e38d6e3868bdedfa241cefbc2e7
Alexander Supertramp <Sanjib-036@github.com>
2021-01-11 21:48:57 +0600

    tabs done

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@
 // let name = prompt('Your name:', ' ');
 // let message = $('<p>').text('Welcome ' + name);
 // $('#welcome').append(message);
-$('.bar ul li').click(function () {
+$('#bar ul li').click(function () {
     $(this).addClass('active').siblings().removeClass('active');
 })
-const tabBtn = document.querySelectorAll('.bar');
-const tab = document.querySelectorAll('.tab');
+const tabBtn = document.querySelectorAll('#bar');
+const tab = document.querySelectorAll('.dashboard-body>.tab');
 
 function tabs(panelIndex) {
     tab.forEach(function (node) {
